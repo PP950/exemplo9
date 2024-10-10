e86e09026970be419a796b2de949491bc5f40de1
Paulo <paulopocas20@gmail.com>
2024-10-10 09:41:44 -0300

    exemplo9

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import Error from './routes/Error/Error';
 import Home from './routes/Home/Home';
 import Produtos from './routes/Produtos/Produtos';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/produtos', element: <Produtos /> },
+    ],
+  },
+]);
 
-const router = createBrowserRouter([{
-    path:'/', element:<App/>,
-    errorElement:<Error/>,
-
-    children:[
-        {path:'/', element:<Home/>},
-        {path:'/produtos', element:<Produtos/>},
-    ]
-
-}])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <RouterProvider router={router} />
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,8 +1,31 @@
-const Home=()=>{
-    return(
+import { useState, useEffect } from "react";
+
+const Home = () => {
+    const [listaproduto, setListaProduto] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5000/produtos")
+            .then((res) => res.json())
+            .then((data) => {
+                setListaProduto(data);
+            })
+            .catch((error) => {
+                console.error("Erro ao buscar produtos:", error);
+            });
+    }, []);
+
+    return (
         <>
-        <h1>Home</h1>
+            <div>
+                {listaproduto.map((item, index) => (
+                    <div key={index}>
+                        <h2>{item.nome}</h2>
+                        <img src={item.foto} alt={item.nome} />
+                    </div>
+                ))}
+            </div>
         </>
-    )
-}
-export default Home
+    );
+};
+
+export default Home;
